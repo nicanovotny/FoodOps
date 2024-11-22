87852e430fe07d5b8f83e0bbeee50e137dfb15e4
nicanovotny <nicanornovotny@gmail.com>
2024-11-22 19:41:47 -0300

    postOrder deleteOrder

--- backend/src/controllers/Order.ts
+++ backend/src/controllers/Order.ts
@@ -18,3 +18,64 @@ export const getOrdersByRestaurant = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'An error occurred while retrieving orders' });
     }
 };
+
+
+export const postOrder = async (req: Request, res: Response) => {
+    try {
+        const { restaurantId } = req.params; // ID del restaurante pasado como parámetro
+        const { products } = req.body; // Productos seleccionados enviados en el cuerpo de la solicitud
+
+        // Validar que se haya enviado al menos un producto
+        if (!Array.isArray(products) || products.length === 0) throw new Error('Products are required');
+
+        // Buscar el restaurante
+        const restaurant = await Restaurant.findById(restaurantId);
+        if (!restaurant) throw new Error('Restaurant not found');
+
+        // Calcular el total
+        const total = products.reduce((sum, product) => sum + product.price, 0);
+
+        // Crear la orden
+        const newOrder = { products: products, total };
+
+        // Guardar la orden en el restaurante
+        restaurant.orders.push(newOrder);
+        await restaurant.save();
+
+        res.status(201).json({
+            message: "Order created successfully",
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "An error occurred while creating the order" });
+    }
+};
+
+export const deleteOrder = async (req: Request, res: Response) => {
+    try {
+        const { restaurantId, orderId } = req.params;
+
+        // Buscar el restaurante
+        const restaurant = await Restaurant.findById(restaurantId);
+
+        if (!restaurant) throw new Error('Restaurant not found');
+
+        // Buscar la orden dentro del restaurante
+        const orderIndex = restaurant.orders.findIndex(
+            (order) => order._id.toString() === orderId
+        );
+
+        if (orderIndex === -1) throw new Error('Order not found');
+
+        // Eliminar la orden
+        restaurant.orders.splice(orderIndex, 1);
+
+        // Guardar los cambios
+        await restaurant.save();
+
+        res.status(200).json({ message: 'Order deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred while deleting the order' });
+    }
+};
