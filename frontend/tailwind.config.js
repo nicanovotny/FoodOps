caa87e3b86f16dd851792ad12b40f5a04533c734
nicanovotny <nicanornovotny@gmail.com>
2024-11-25 18:16:40 -0300

    Cleaning

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -25,10 +25,10 @@ module.exports = {
               "base-100": "#F5F5F3", // Main light background
               "base-200": "#EDE6DB", // Secondary light background
               "base-300": "#D6CEC3", // Tertiary background
-              info: "#A38C79", // Informative
-              success: "#928D72", // Success
-              warning: "#C1A56B", // Warning
-              error: "#A45B41", // Error
+              info: "#A38C79", 
+              success: "#928D72", 
+              warning: "#C1A56B", 
+              error: "#A45B41",
               delete: "#B02A0B",
             },
           },
